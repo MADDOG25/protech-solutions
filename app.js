2426a0a4746221515d4ba81c9e8e1cf09cb142b6
Dev Jeffrey <mdog1525@gmail.com>
2024-04-11 17:53:56 -0500

    modified app

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ menu.addEventListener("click", function () {
   menuList.classList.toggle("nav_list--show");
 });
 
+links.forEach(function(link){
     link.addEventListener('click', function(){
         menuList.classList.remove('nav_list--show');
     })
