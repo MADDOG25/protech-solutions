bc94adc171c177800b1d07dad045a43dd28e9829
Dev Jeffrey <mdog1525@gmail.com>
2024-04-11 16:53:00 -0500

    cambio de logo

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ menu.addEventListener("click", function () {
   menuList.classList.toggle("nav_list--show");
 });
 
-links.forEach(function(link){
     link.addEventListener('click', function(){
         menuList.classList.remove('nav_list--show');
     })
